564695932f139c8391f15314a84a9991e1ab3320
Elizabeth Rudaya <lizarudayaa@gmail.com>
2021-12-12 11:48:56 +0200

    group2

--- src/assets/data/students.js
+++ src/assets/data/students.js
@@ -1,16 +1,32 @@
+// const students = [
+// 'Artem Vinnytskyi',
+// 	'Denys Stepanenko',
+// 	'Mykhailo Polevnychyi',
+// 	'Oleksandr Dovhaniuk',
+// 	'Oleksandr Markiv',
+// 	'Oleksandr Prokopenko',
+// 	'Oleksandr Prosvietov',
+// 	'Oleksii Hezha',
+// 	'Roman Babych',
+// 	'Tymur Yakovenko',
+// 	'Valeriia Hryhorenko',
+// 	'Vladyslav Zarytskyi'
+//
+// ]
+
 const students = [
-'Artem Vinnytskyi',
-	'Denys Stepanenko',
-	'Mykhailo Polevnychyi',
-	'Oleksandr Dovhaniuk',
-	'Oleksandr Markiv',
-	'Oleksandr Prokopenko',
-	'Oleksandr Prosvietov',
-	'Oleksii Hezha',
-	'Roman Babych',
-	'Tymur Yakovenko',
-	'Valeriia Hryhorenko',
-	'Vladyslav Zarytskyi'
+	'Andrii Doroshenko',
+	'Artem Dolia',
+	'Artem Tsaturian',
+	'Danylo Bychok',
+	'Dmytro Ihnatushyn',
+	'Kyrylo Utin',
+	'Mariia Pisarenko',
+	'Mariia Suprun',
+	'Sofiia Viazovska',
+	'Tymur Semenov',
+	'Volodymyr Diemieniev',
+	'Yevhenii Dedion'
 
 ]
 
